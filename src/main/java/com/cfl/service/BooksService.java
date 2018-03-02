9220397d072e30e0dda89fead5a8aa007b6c75ec
ndcfl <275300091@qq.com>
2018-03-02 18:11:56 +0800

    盛达兴后台管理

---
+++ src/main/java/com/cfl/service/BooksService.java
@@ -0,0 +1,10 @@
+package com.cfl.service;
+
+import com.cfl.vo.BooksVo;
+import com.cfl.vo.Select2Vo;
+
+import java.util.List;
+
+public interface BooksService extends BaseService<BooksVo>{
+    List<Select2Vo> getBooks();
+}
